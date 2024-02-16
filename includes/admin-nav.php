8e49bc64bd42eaa1591ba8ae0a6ea47a482ee72f
ritika8803 <ritika.bhardwaj2021@vitbhopal.ac.in>
2024-02-16 16:02:24 +0530

    "bye bye"

--- includes/admin-nav.php
+++ includes/admin-nav.php
@@ -1,38 +1,37 @@
-<div class="menu">
-            <table class="menu-container" border="0">
-                <tr>
-                    <td style="padding:10px" colspan="2"  class="profile-container flex justify-center items-center my-5">
-                        
-                            <img src="../img/logo.jpg" width="200px" class="">
-                    
-                    </td>
-                
-                </tr>
-                <tr class="menu-row" >
-                    <td class="menu-btn <?php if ($page == "home"): ?> menu-active menu-icon-dashbord-active <?php else: ?> menu-icon-dashbord <?php endif; ?>" >
-                        <a href="index.php" class="non-style-link-menu <?php if ($page == "home"): ?>non-style-link-menu-active <?php endif; ?>"><div><p class="menu-text">Dashboard</p></a></div></a>
-                    </td>
-                </tr>
-                <tr class="menu-row">
-                    <td class="menu-btn menu-icon-doctor ">
-                        <a href="doctors.php" class="non-style-link-menu "><div><p class="menu-text">Doctors</p></a></div>
-                    </td>
-                </tr>
-                <tr class="menu-row" >
-                    <td class="menu-btn menu-icon-schedule ">
+<div class="menu">    
+            <div class="menu-container" border="0">
+                    <div style="padding:10px" class="profile-container1 flex justify-center items-center my-5">  
+                        <img src="../img/logo.jpg" width="200px" class="">
+                    </div>
+                    <div class="menu-btn menu-icon-dashbord" >
+                        <a href="index.php" class="non-style-link-menu"><div><p class="menu-text">Dashboard</p></a></div></a>
+                    </div>
+                    <div class="menu-btn menu-icon-doctor menu-active menu-icon-doctor-active">
+                        <a href="doctors.php" class="non-style-link-menu non-style-link-menu-active"><div><p class="menu-text">Doctors</p></a></div>
+                    </div>
+                    <div class="menu-btn menu-icon-schedule">
                         <a href="schedule.php" class="non-style-link-menu"><div><p class="menu-text">Schedule</p></div></a>
-                    </td>
-                </tr>
-                <tr class="menu-row">
-                    <td class="menu-btn  <?php if ($page == "appointment"): ?> menu-active menu-icon-appoinment-active <?php else: ?> menu-icon-appoinment <?php endif; ?>">
-                        <a href="appointment.php" class="non-style-link-menu <?php if ($page == "appointment"): ?>non-style-link-menu-active <?php endif; ?>"><div><p class="menu-text">Appointment</p></a></div>
-                    </td>
-                </tr>
-                <tr class="menu-row" >
-                    <td class="menu-btn menu-icon-patient">
+                    </div>
+                    <div class="menu-btn menu-icon-appoinment">
+                        <a href="appointment.php" class="non-style-link-menu"><div><p class="menu-text">Appointment</p></a></div>
+                    </div>
+                    <div class="menu-btn menu-icon-patient">
                         <a href="patient.php" class="non-style-link-menu"><div><p class="menu-text">Patients</p></a></div>
-                    </td>
-                </tr>
+                    </div>
+                    <div style="padding:10px" colspan="2">
+                        <div class="profile-container">
+                                <div class="profile-subContainer">   
+                                <img src="../img/user.png" width="22%" style="border-radius:50%">
+                                <div class="profile-subSubContainer">
+                                <p class="profile-title">Administrator</p>
+                                <p class="profile-subtitle">admin@edoc.com</p>
+                                </div>
+                                </div>
+                                <div>
+                                <a href="../logout.php" ><input type="button" value="Log out" class="logout-btn btn-primary-soft btn"></a>
+                                </div>
+                        </div>
+                    </div>
 
-            </table>
+            </div>
         </div>
